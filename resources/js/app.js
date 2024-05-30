14d529b215a3007b44b4aaff6899f79457f54a66
viksj <viksjangid1@gmail.com>
2024-05-30 17:57:42 +0530

    Add Sweet alert

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,5 @@ import '../css/app.css';
 import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js';
 
 import './components/App';
+
+import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js';
--- resources/js/components/app/index.jsx
+++ resources/js/components/app/index.jsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from "react";
 import { InputField, TextArea } from "./components/formFieldComponents/Input";
+import Swal from "sweetalert2";
 
 export default function Index() {
     const [taskName, setTaskName] = useState('');
@@ -25,6 +26,45 @@ export default function Index() {
 
     }
 
+    const handleDoneTask = () => {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "success",
+            title: "Signed in successfully"
+          });
+    }
+
+    const handleDeleteTask = () => {
+        Swal.fire({
+            title: "Are you sure?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+          });
+          
+          
+    }
+
     return (
         <div className="container">
             <div className="row">
@@ -37,6 +77,28 @@ export default function Index() {
                             <InputField label="Task Name" type="text" value={taskName} onChange={handleChangeTaskName} error={error?.taskName} />
                             <TextArea label="Task Discrition" value={taskDiscription} onChange={handleChangeTaskDiscription} error={error?.taskDiscription} />
                             <button type="button" class="btn btn-sm btn-primary float-end" onClick={handleAddTask}>Add Task</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 mt-3">
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="card">
+                                <div className="card-body">
+                                    <div className="row">
+                                        <div className="col-8">
+                                            <h6 className="card-title">Task Name</h6>
+                                            <p className="card-text">
+                                                sdasdas
+                                            </p>
+                                        </div>
+                                        <div className="col-4 d-flex ">
+                                            <button className="btn btn-sm" onClick={handleDoneTask}><i className="fa-solid fa-check"></i></button>
+                                            <button className="btn btn-sm" onClick={handleDeleteTask}><i className="fa-regular fa-trash-can"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
